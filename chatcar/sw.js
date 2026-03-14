8ac6715dd07cb2681de42b8072f97b546e9d7fd5
javar115 <107451238+javar115@users.noreply.github.com>
2026-03-14 17:33:30 -0300

    Update sw.js

--- chatcar/sw.js
+++ chatcar/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'chatcar-v1';
-const FILES = ['./index.html', './manifest.json'];
+const CACHE = 'chatcar-v4';
+const FILES = ['./index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(
@@ -19,6 +19,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
