f9976500577bb509efde6fd0288a06216844f613
javar115 <107451238+javar115@users.noreply.github.com>
2026-03-19 11:34:42 -0300

    Update sw.js

--- chatcar/sw.js
+++ chatcar/sw.js
@@ -1,28 +1,9 @@
-const CACHE = 'chatcar-v7';
-
-// Network first - always try to get fresh version
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+// Service Worker desactivado - desregistra cualquier versión anterior
+self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
   self.clients.claim();
-});
-
-// Always fetch from network, fall back to cache
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(e.request))
-  );
 });
